27c657a7878731d6204af4078ed4a2976b64138d
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2022-12-23 14:53:26 +0000

    code container

---
+++ Array/linearSearch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int *arr, int n, int x) {
+    for(int i=0; i<n; i++) {
+        if(arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+int main() {
+    int n,x;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    cin>>x;
+    cout<<linearSearch(arr, n, x);
+
+    delete arr;
+    return 0;
+}
