70a088c278a8230847f9d47dfc262e013a66a6bc
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2022-12-24 13:03:27 +0000

    code container

---
+++ Array/arraySum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int arraySum(int *arr, int n) {          //Time Complexity = O(n)
+    int sum = 0;
+    for(int i=0; i<n; i++) 
+        sum += arr[i];
+    return sum;
+}
+int main() {
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    cout<<arraySum(arr, n);
+
+    delete arr;
+    return 0;
+}
