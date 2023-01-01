0f63b4a0b4eb80b24a6b28edeae5815e4b0c5554
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-01 04:53:15 +0000

    pointer arithmetic decrement operator code added

---
+++ Pointers/4. pointer-arithmetic/2.decrementOperator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    float y = 5.5f;
+    int *iptr = &x;
+    float *fptr = &y;
+
+    cout << &x << "    " << iptr << "\n"; 
+    cout << x << "    " << *iptr << "\n"; 
+    cout << &y << "    " << fptr << "\n"; 
+    cout << y << "    " << *fptr << "\n\n"; 
+    
+    (*iptr)--;
+    (*fptr)--;
+
+    cout << &x << "    " << iptr << "\n"; 
+    cout << x << "    " << *iptr << "\n"; 
+    cout << &y << "    " << fptr << "\n"; 
+    cout << y << "    " << *fptr << "\n\n"; 
+    
+    iptr--;
+    fptr--;
+
+    cout << &x << "    " << iptr << "\n"; 
+    cout << x << "    " << *iptr << "\n"; 
+    cout << &y << "    " << fptr << "\n"; 
+    cout << y << "    " << *fptr << "\n\n"; 
+
+    return 0;
+}
