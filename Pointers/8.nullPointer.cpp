389405a038931619673a5364095e2165b1d5082f
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-02 10:51:57 +0530

    null pointer example code added

---
+++ Pointers/8.nullPointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *iptr1 = NULL;
+    int *iptr2 = (int*)0;
+    int *iptr3 = 0;      //Only for int
+    
+    char *cptr1 = NULL;
+    char *cptr2 = (char*)0;
+    
+    float *fptr1 = NULL;
+    float *fptr2 = (float*)0;
+
+    return 0;
+}
