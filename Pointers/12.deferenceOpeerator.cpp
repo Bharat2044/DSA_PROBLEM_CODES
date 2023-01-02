19bdcceae64c7275391e92d99ed42660369875ae
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-02 11:02:24 +0530

    deference operator example code added

---
+++ Pointers/12.deferenceOpeerator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int *ptr = &x;
+    cout << x << endl;
+    cout << *ptr << endl;
+    cout << *(&x) << endl;
+    cout << *(&(*ptr)) << endl;
+    
+    //NOTE: * is  deference operator, which is used to ectch the value of the address
+
+    return 0;
+}
