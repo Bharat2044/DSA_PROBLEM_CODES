a771699d5aa4ba1cc5beb78bd7d63eee3a411cd1
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-04 05:27:46 +0530

    swap 2 number.cpp

---
+++ Functions/3.call-by-reference/2.swap2Number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+int main() {
+    int x = 10;
+    int y = 20;
+    cout << "Before swaped values are:\n";
+    cout << x <<"    " << y << endl;
+    swap(x, y);
+    cout << "After swaped values are:\n";
+    cout << x <<"    " << y << endl;
+
+    return 0;
+}
