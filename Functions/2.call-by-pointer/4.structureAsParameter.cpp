b5c61ea6f610910e8977b30124033d9eff7a11e0
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-04 06:14:25 +0530

    structure as parameter code added

---
+++ Functions/2.call-by-pointer/4.structureAsParameter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+struct Rectangle {
+    int length;
+    int breadth;
+};
+int area(Rectangle *r1) {
+    (*r1).length++;    //r1->length++;
+    return r1->length * r1->breadth;
+}
+int main() { 
+    Rectangle r = {10, 5};
+    cout << area(&r) << endl;
+    
+    return 0;
+}
