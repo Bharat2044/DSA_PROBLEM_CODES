44291271d9b7c943ed51d8a633cfb91da2109200
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-04 06:29:16 +0530

    passing structure of array as an argument code

---
+++ Functions/3.call-by-reference/5.passingArrayAsStructure.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+struct Test {
+    int arr[5];
+    int n;
+};
+void fun(Test &t1) {
+    for(int i=0; i<t1.n; i++) {
+        cout << t1.arr[i] << "    ";
+        t1.arr[i] = i+11;
+    }
+    cout<<endl;
+}
+int main() { 
+    Test t = {{2, 4, 6 ,8,10}, 5};
+    fun(t);
+    for(int i=0; i<t.n; i++) 
+        cout << t.arr[i] << "    ";
+    
+    return 0;
+}
