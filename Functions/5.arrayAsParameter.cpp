c0cb402b1c498c5cb7606b3ed7874299bcd6a3b8
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-04 05:52:52 +0530

    array as parameter code added

---
+++ Functions/5.arrayAsParameter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++)
+        cout << arr[i] << endl;
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, n);
+
+    return 0;
+}
