e50af2456f87e02ac2a71c08871536da4d5800ec
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-07 19:44:13 +0530

    sum of array element code added

--- Array/arraySum.cpp
+++ Array/arraySum.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int arraySum(int *arr, int n) {          //Time Complexity = O(n)
+int arraySum(int *arr, int n) {          // Time Complexity = O(n)
     int sum = 0;
     for(int i=0; i<n; i++) 
         sum += arr[i];
