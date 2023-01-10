1876115d3b1d323fdf23564fc68ff37a7c358ee1
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-10 11:40:57 +0000

    print array element code added

---
+++ Array/2. printArrayElement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {     //Time Complexity = O(n)
+    for(int i=0; i<5; i++) {
+        cout << arr[i] << "   ";            //You can use any ways to print array element
+        //cout << i[arr] << "   ";
+        //cout << *(arr+i) << "   ";
+        //cout << *(i+arr) << "   ";
+    }
+        
+}
+int main() {            
+    int arr[] = {2, 4, 6, 8, 10};             //Array Declaration + Initialization
+    printArray(arr, 5);
+    
+    return 0;
+}
