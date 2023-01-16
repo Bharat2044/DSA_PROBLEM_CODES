3d1a01cf252a346150ad83ec2474716aa2cfcd58
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-01-16 05:48:21 +0000

    get or access particular index element code added

---
+++ Array/07.array-operations/05.get.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int get(int *arr, int n, int index) {          //Time Complexity = O(n)
+    if(index>=0 && index<n)
+        return arr[index];
+    return -1;
+}
+int main() {
+    int n, index;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int *arr = new int[n];
+    cout << "Enter array elements: ";
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    cout << "Enter index number want to find element: ";
+    cin >> index;
+    cout << get(arr, n, index);
+
+    delete []arr;
+    return 0;
+}
