70646b571f545d2ea9c9172bb0fff1d0e535b236
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-12 15:10:36 +0000

    hollow inverted half pyramid code

---
+++ Loops/7.Patterns/pattern08.cpp
@@ -0,0 +1,27 @@
+/*
+    n = 5
+    * * * * *
+    *     *
+    *   *
+    * *
+    *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i+1; j++) {
+            if((i==1) || (j==n-i+1) || (j==1))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
