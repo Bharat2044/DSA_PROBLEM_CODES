1d915030f2faa6106eb46a5e763d7f7474740c62
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-12 15:08:03 +0000

    hollow rectangle code

---
+++ Loops/7.Patterns/pattern04.cpp
@@ -0,0 +1,25 @@
+/*
+    m = 3, n = 5
+    * * * * *
+    *       *
+    * * * * *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    for(int i=1; i<=m; i++) {
+        for(int j=1; j<=n; j++) {
+             if((i==1) || (i==m) || (j==1)|| (j==n))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
