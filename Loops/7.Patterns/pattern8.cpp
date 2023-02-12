0d2c4d8f5d05ddfde35b22b0cc81ee78bae360fd
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-12 14:54:34 +0000

    pattern8 code

---
+++ Loops/7.Patterns/pattern8.cpp
@@ -0,0 +1,26 @@
+/*
+    n = 5
+* * * * *
+  * * * * 
+    * * * 
+      * * 
+        *  
+*/ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        //for space
+        for(int s=1; s<=i-1; s++)
+            cout << "  ";
+        for(int j=1; j<=n-i+1; j++)
+            cout << "* ";
+        cout << endl;
+    }
+
+    return 0;
+}
