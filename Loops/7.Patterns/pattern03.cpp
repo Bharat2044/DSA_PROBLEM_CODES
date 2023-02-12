87113967b8f11c7687bc1daf1b581f52577e22fc
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-12 15:07:33 +0000

    solid rectangle code

---
+++ Loops/7.Patterns/pattern03.cpp
@@ -0,0 +1,22 @@
+/*
+    m = 3, n = 5
+    * * * * *
+    * * * * *
+    * * * * *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    for(int i=1; i<=m; i++) {
+        for(int j=1; j<=n; j++)
+            cout << "* ";
+        cout << endl;
+    }
+
+    return 0;
+}
