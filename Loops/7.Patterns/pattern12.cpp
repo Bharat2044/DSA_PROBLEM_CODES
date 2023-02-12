33a99e995b623c18906a9912d2349e2df08bacff
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-12 15:18:07 +0000

    pattern 12 code

---
+++ Loops/7.Patterns/pattern12.cpp
@@ -0,0 +1,29 @@
+/*
+    n = 5
+* * * * *
+  *     * 
+    *   * 
+      * * 
+        *  
+*/ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        for(int s=1; s<=i-1; s++)
+            cout << "  ";
+        for(int j=1; j<=n-i+1; j++) {
+            if((i==1) || (j==1) || (j==n-i+1))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
