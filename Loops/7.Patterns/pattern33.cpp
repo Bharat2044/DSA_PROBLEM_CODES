742f9167b80c2dbf25a06942e087471580250023
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-15 15:18:53 +0000

    number palindrome full pyramid code

---
+++ Loops/7.Patterns/pattern33.cpp
@@ -0,0 +1,32 @@
+/*
+    n = 5
+        1
+      1 2 1
+    1 2 3 2 1
+  1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        //for space 
+        for(int s=1; s<=n-i; s++)
+            cout << "  ";
+        //left part
+        for(int j=1; j<=i; j++)
+            cout << j << " ";
+        
+        //right part
+        for(int k=2; k<=i;k++)
+            cout << i-k+1 << " ";
+        cout << endl;
+    }
+    
+    return 0;
+}
