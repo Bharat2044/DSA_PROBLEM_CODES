c9453b9ad627d4aa8b65e006ad2a314b0f2fc047
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-15 11:11:11 +0000

    hollow diamond code

---
+++ Loops/7.Patterns/pattern18.cpp
@@ -0,0 +1,46 @@
+/*
+ n = 5
+    *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    // upper half
+    for(int i=1; i<=n; i++) {
+        for(int s=1; s<=n-i+1; s++)
+            cout << " ";
+        for(int j=1; j<=i; j++) {
+            if((j==1) || (j==i))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    // lower half
+    for(int i=n-1; i>=1; i--) {
+        for(int s=1; s<=n-i+1; s++)
+            cout << " ";
+        for(int j=1; j<=i; j++) {
+            if((j==1) || (j==i))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
