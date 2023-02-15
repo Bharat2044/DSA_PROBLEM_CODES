c7b3ac33789b640d49a7fe1717bcad7f5dc7eacc
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-15 11:30:03 +0000

    number solid square code

---
+++ Loops/7.Patterns/pattern21.cpp
@@ -0,0 +1,22 @@
+/*
+ n = 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) 
+            cout << j << " ";
+        cout << endl;
+    }
+    return 0;
+}
