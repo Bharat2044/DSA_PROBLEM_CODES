51c7c48b9b16f1090b981f907fafec07ce46fbd1
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-15 10:50:00 +0000

    hollow full pyramid code

---
+++ Loops/7.Patterns/pattern14.cpp
@@ -0,0 +1,29 @@
+/*
+    n = 5
+    *
+   * *
+  *   *
+ *     *
+* * * * *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        for(int s=1; s<=n-i; s++)
+            cout << " ";
+        for(int j=1; j<=i; j++) {
+            if((i==n) || (j==1) || (i==j))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
