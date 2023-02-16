6957c04db63c923368f8d95ce42568acb4e0f0b6
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-16 11:52:49 +0000

    solid rhombus code

---
+++ Loops/7.Patterns/pattern48.cpp
@@ -0,0 +1,26 @@
+/*
+n = 5
+    * * * * * 
+   * * * * * 
+  * * * * * 
+ * * * * * 
+* * * * *    
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++) {
+        for(int s=1; s<=n-i;s++)
+            cout << " ";
+        for(int j=1; j<=n; j++)
+            cout << "* ";
+        cout << endl;
+    }
+    
+    return 0;
+}
