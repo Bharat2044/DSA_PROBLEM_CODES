b86d61f402f503c0546aa2bc4f525e9d617d59af
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-16 12:14:43 +0000

    alphabet hollow half pyramid code

---
+++ Loops/7.Patterns/pattern51.cpp
@@ -0,0 +1,29 @@
+/*
+n = 5
+A 
+A B 
+A   C 
+A     D 
+A B C D E
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++) {
+        
+        for(int j=1; j<=i; j++) {
+            if(i==n || j==1 || j==i)
+                cout << (char)('A'+j-1) << " ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
