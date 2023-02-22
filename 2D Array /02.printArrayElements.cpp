57f1e416b79f5a071398c60b738fa978459b83ef
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-22 12:06:14 +0000

    print 2d array elements in row and column order

---
+++ 2D Array /02.printArrayElements.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void printRowMajorOrder(int arr[][3], int m, int n) {
+    cout << "Array A printing in row major order:\n";      
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++)
+            cout << arr[i][j] << "  ";
+        cout << endl;
+    }
+    cout << endl;
+}
+void printColumnMajorOrder(int arr[][3], int m, int n) {
+    cout << "Array A printing in column major order:\n";      
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++)
+            cout << arr[j][i] << "  ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    printRowMajorOrder(arr, 3, 3);
+    printColumnMajorOrder(arr, 3, 3);
+    
+    return 0;
+}
