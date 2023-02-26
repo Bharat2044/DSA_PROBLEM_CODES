787605d1be0e7a6b28ab088bb902e9feaa7361ae
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-26 11:02:26 +0000

    right rotate by one position code

--- Array/23.rightRotateByOnePosition.cpp
+++ Array/23.rightRotateByOnePosition.cpp
@@ -1,5 +1,5 @@
-// i/p : arr[] = {1, 2, 3, 4, 5};
-// o/p : arr[] = {2, 3, 4, 5, 1}
+// i/p : arr[] = {1, 2, 3, 4, 5}
+// o/p : arr[] = {5, 1, 2, 3, 4}
 
 #include <iostream>
 using namespace std;
@@ -11,25 +11,25 @@ void printArray(int arr[], int n) {
 }
 
 // Time Complexity = O(n), Space Complexity = O(1)
-void leftRotateByOnePosition(int arr[], int n) {
-    int x = arr[0];
+void rightRotateByOnePosition(int arr[], int n) {
+    int x = arr[n-1];
     
     // shifting 
-    for(int i = 1; i<n; i++)
-        arr[i-1] = arr[i];
-    arr[n-1] = x;
+    for(int i=n-1; i>=1; i--)
+        arr[i] = arr[i-1];
+    arr[0] = x;
 }
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     
-    cout << "Before left rotate by 1 position array are:\n";
+    cout << "Before right rotate by 1 position array are:\n";
     printArray(arr, n);
     
-    leftRotateByOnePosition(arr, n);
+    rightRotateByOnePosition(arr, n);
     
-    cout << "\nAfter left rotate by 1 position array are:\n";
+    cout << "\nAfter right rotate by 1 position array are:\n";
     printArray(arr, n);
 
     return 0;
