e9f8e5f04c126a905ba2230bef037e18a0073d04
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 12:29:19 +0000

    sizeof all datatype code

---
+++ Data Types & Type Casting/3.sizeOfDataType/sizeOfDataType.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Size of following Datatypes in bytes: \n";
+    cout << "int : " << sizeof(int) << endl;
+    cout << "unsigned int : " << sizeof(unsigned int) << endl;
+    cout << "short int : " << sizeof(short int) << endl;
+    cout << "long int : " << sizeof(long int) << endl;
+    cout << "unsigned short int : " << sizeof(unsigned short int) << endl;
+    cout << "unsigned long int : " << sizeof(unsigned long int) << endl;
+    cout << "long long int : " << sizeof(long long int) << endl;
+    cout << "unsigned long long int : " << sizeof(unsigned long long int) << endl;
+    cout << "signed char : " << sizeof(signed char) << endl;
+    cout << "unsigned char : " << sizeof(unsigned char) << endl;
+    cout << "wchar_t : " << sizeof(wchar_t) << endl;
+    cout << "float : " << sizeof(float) << endl;
+    cout << "double : " << sizeof(double) << endl;
+
+    return 0;
+}
