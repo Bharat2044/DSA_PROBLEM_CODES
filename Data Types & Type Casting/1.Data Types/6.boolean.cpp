4d75a24bebc34ad58c471743a382c2dfd9459f8e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 12:18:20 +0000

    boolean data type code

---
+++ Data Types & Type Casting/1.Data Types/6.boolean.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    bool a = true;
+    cout << "a = " << a << endl;
+    
+    bool b = false;
+    cout << "b = " << b << endl;
+    
+    bool c = 1;
+    cout << "c = " << c << endl;
+    
+    bool d = 0;
+    cout << "d = " << d << endl;
+    
+    bool e = 2343;
+    cout << "e = " << e << endl;
+    
+    bool f = -23;
+    cout << "f = " << f << endl;
+    
+    bool g = -0;
+    cout << "g = " << g << endl;
+    
+    
+    return 0;
+}
