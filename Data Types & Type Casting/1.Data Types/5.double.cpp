991fd8c22818699fca0b91948cbf73d40d2765fc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 12:15:08 +0000

    double data type code

---
+++ Data Types & Type Casting/1.Data Types/5.double.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double a = 10;
+    cout << "a = " << a << endl;
+    
+    double b = 20.0;
+    cout << "b = " << b << endl;
+    
+    double c = 47.4045;
+    cout << "c = " << c << endl;
+    
+    double d = 647.78968;
+    cout << "d = " << d << endl;
+    
+    
+    return 0;
+}
