c716f98503d108f40ebb1c21af18cc603bc22643
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 12:14:59 +0000

    float data type code

---
+++ Data Types & Type Casting/1.Data Types/4.float.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float a = 10;
+    cout << "a = " << a << endl;
+    
+    float b = 20.0f;
+    cout << "b = " << b << endl;
+    
+    float c = 47.4045f;
+    cout << "c = " << c << endl;
+    
+    float d = 647.78968f;
+    cout << "d = " << d << endl;
+    
+    
+    return 0;
+}
