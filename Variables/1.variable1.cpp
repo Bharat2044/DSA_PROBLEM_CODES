38d25c6988be75d9aca7d6896ba12994e1d2a198
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 11:46:52 +0000

    declaring and initializing single variable code

---
+++ Variables/1.variable1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;          // Declaration
+    a = 5;          // Initialization
+    int b = 10;    // Declaration + Initialization
+
+    cout << a << endl;
+    cout << b << endl;
+
+    int myAge = 20;
+    cout << "I am " << myAge << " years old." << endl;
+
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+
+    return 0;
+}
