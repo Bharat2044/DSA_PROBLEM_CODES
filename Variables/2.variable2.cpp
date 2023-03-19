618c18ad006757c879387cfd3f073ddc824651d5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 11:49:16 +0000

    declaring and initializing multiple variable code

---
+++ Variables/2.variable2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 50;
+    cout << a+b+c << endl;
+
+    int x, y, z;
+    x = y = z = 50;
+    cout << x+y+z << endl;
+    
+    return 0;
+}
