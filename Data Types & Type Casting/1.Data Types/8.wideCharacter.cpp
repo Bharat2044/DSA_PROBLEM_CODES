bc228fec42c1a5bc3c11846a64243ad8aae1aa94
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-19 12:25:29 +0000

    wide character data type code

---
+++ Data Types & Type Casting/1.Data Types/8.wideCharacter.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    wchar_t a = 'A';
+    cout << "a = " << a << endl;
+    
+    wchar_t b = L'A';
+    cout << "b = " << b << endl;
+    
+    wchar_t c = 'AB';
+    cout << "a = " << c << endl;
+    
+    wchar_t d = L'AB';
+    cout << "d = " << d << endl;
+    
+    
+    return 0;
+}
