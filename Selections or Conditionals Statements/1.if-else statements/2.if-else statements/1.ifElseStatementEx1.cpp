75421bfc1696b54649bd6c016bdc7031a6d62d46
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 19:59:35 +0000

    if else statement code

---
+++ Selections or Conditionals Statements/1.if-else statements/2.if-else statements/1.ifElseStatementEx1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if(true)
+        cout << "Hi" << endl;
+    else
+        cout << "Hello" << endl;
+        
+        
+    if(false)
+        cout << "Bharat" << endl;
+    else
+        cout << "Rahul" << endl;
+
+    return 0;
+}
