7c212ca2ca86afd5e762996ad194513ce3ed00c3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 06:21:06 +0000

    arithmetic binary operator code

---
+++ Operators/1.Arithmetic Operators/2.arithmeticBinaryOperators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 7;
+    int num2 = 3;
+
+    // Arithmetic Addition
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    // Arithmetic Subtraction
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+
+    // Arithmetic Multiplication
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+
+    // Arithmetic Division
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+
+    // Arithmetic Modulus
+    cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+
+    return 0;
+}
