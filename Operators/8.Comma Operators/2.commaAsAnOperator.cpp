b3920b7a757089190a085b480a2ec6f2371ba2a6
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 07:21:31 +0000

    comma as an operator code

---
+++ Operators/8.Comma Operators/2.commaAsAnOperator.cpp
@@ -0,0 +1,13 @@
+// Syntax : data_type variable = (value1, value2, ..., valueN);
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// comma as a operator
+	int num = (24, 78, 85);
+
+	cout << num << endl;
+
+	return 0;
+}
