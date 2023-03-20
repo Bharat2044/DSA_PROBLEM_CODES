a21f06c1576cc9d4d364b2a11416c6f317fa9ff4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 06:38:02 +0000

    bitwise unary operator code

---
+++ Operators/4.Bitwise Operators/1.bitwiseUnaryOperators.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // bitwise NOT / Complement operator
+    cout << "~" << "10" << " is " << ~10 << endl;
+    cout << "~" << "(-10)" << " is " << ~(-10) << endl;
+
+    return 0;
+}
