0e96e45bcd2200dfb2dbcba50a3aff1b93343606
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 20:16:33 +0000

    if-else-if ladder code

---
+++ Selections or Conditionals Statements/1.if-else statements/4.if-else-if ladder/1.ifElseIfLadderEx1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if(false)
+        cout << "Hi" << endl;
+    else if(true)
+        cout << "Hello" << endl;
+    else
+        cout << "Hola" << endl;
+        
+    return 0;
+}
