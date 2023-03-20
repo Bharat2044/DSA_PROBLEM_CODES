2d0ae134efd1666e3000ba0f15fd3fbe26e5f152
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 20:25:08 +0000

    nested if statement code

---
+++ Selections or Conditionals Statements/1.if-else statements/3.nested if statements/1.nestedIfStatementEx1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if(true) {
+        if(false)
+            cout << "Hi" << endl;
+
+        if(true)
+            cout << "Hello" << endl;
+            
+        cout << "Hola" << endl;
+    }
+        
+    return 0;
+}
