98a79f40d460505076dec43c944f707c9afb8597
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 07:26:27 +0000

    comma operator in place of a semicolon code

---
+++ Operators/8.Comma Operators/4.commaOperatorInPlaceOfASemicolon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// variable declaration
+	int num1 = 54;
+	int num2 = 34;
+	int num3 = 45;
+
+	cout << num1 << endl, cout << num2 << endl, cout << num3 << endl;
+
+	return 0;
+}
