6f9934ec91cf3f40f3cd107b489540fb8b3fde9e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 06:05:39 +0000

    character data type code

--- Data Types & Type Casting/1.Data Types/7.character.cpp
+++ Data Types & Type Casting/1.Data Types/7.character.cpp
@@ -6,7 +6,10 @@ int main() {
     cout << "a = " << a << endl;
     
     char b = 'xy';
-    cout << "b = " << b << endl;
+    cout << "b = " << b << endl << endl;
+
+    for(int i = 0; i < 256; i++) 
+        cout << i << " => " << (char)i << endl;
     
     return 0;
 }
