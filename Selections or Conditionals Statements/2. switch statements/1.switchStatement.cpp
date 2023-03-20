48d62f488dda4d408817eb14f7a3fb46564cd60f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-20 20:33:47 +0000

    switch statement code

---
+++ Selections or Conditionals Statements/2. switch statements/1.switchStatement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 2;
+    
+    switch(num) {
+        case 1:
+            cout << "Number is " << num << endl;
+            break;
+            
+        case 2:
+            cout << "Number is " << num << endl;
+            break;
+            
+        case 3:
+            cout << "Number is " << num << endl;
+            break;
+            
+        case 4:
+            cout << "Number is " << num << endl;
+            break;
+            
+        default:
+            cout << "Default case " << endl;
+    }
+    
+    return 0;
+}
