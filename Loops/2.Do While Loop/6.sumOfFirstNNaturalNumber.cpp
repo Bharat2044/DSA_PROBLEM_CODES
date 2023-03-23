00fd7294125d4ad963e50cffcf82df48c81ef218
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-23 21:44:07 +0000

    sum of first N natural number code

---
+++ Loops/2.Do While Loop/6.sumOfFirstNNaturalNumber.cpp
@@ -0,0 +1,27 @@
+// C++ program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    // Approach - 1 => Time Complexity = O(n)
+    /*int i = 1;
+    do {
+        sum += i;
+        ++i;
+    } while (i <= num);*/
+    
+    
+    // Approach - 2 => Time Complexity = O(1)
+    sum = (num*(num+1) / 2);
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
