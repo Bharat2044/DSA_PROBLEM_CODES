15fe18cb19db0a3bdca78d9842a4c621d4955d92
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-23 21:13:50 +0000

    while loop code

---
+++ Loops/1.While Loop/1.whileLoopEx1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1; 
+    
+    while (i <= 5) {
+        cout << "Hello" << endl;
+        ++i;
+    }
+    
+    return 0;
+}
