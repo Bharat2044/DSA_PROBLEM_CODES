e5ff73e6d3b36c997b13c38c93566833f22f1f92
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-23 23:42:34 +0000

    sum of positive number code

--- Loops/4.Break Statements/3.sumOfPositiveNumber.cpp
+++ Loops/4.Break Statements/3.sumOfPositiveNumber.cpp
@@ -1,21 +1,25 @@
+// program to find the sum of positive numbers
+// if the user enters a negative numbers, break ends the loop
+// the negative number entered is not added to sum
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int num;
-    
-    cout << "Enter a positive number: ";
-    cin >> num;
-    
     int sum = 0;
-    while(num) {
-        if(num < 0)
-            break;
-            
-        sum += num;
-        
+
+    while(true) {
+        // take input from the user
         cout << "Enter a positive number: ";
         cin >> num;
+
+        // break condition
+        if(num < 0)
+            break;     
+            
+         // add all positive numbers
+        sum += num;      
     } 
     cout << "Sum = " << sum << endl;
     
