63616e89f7ec4b7968db3a0d91d2b0a20a634deb
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-23 21:16:55 +0000

    do-while loop example code

---
+++ Loops/2.Do While Loop/2.doWhileLoopEx2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5; 
+    
+    do {
+        cout << "Hello" << endl;
+        i--;
+    } while (i >= 1);
+    
+    return 0;
+}
