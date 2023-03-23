e8db22ded571019276f4aa812d3ee114bfabbf07
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-23 14:27:17 +0000

    minimum between 2 number code

---
+++ Selections or Conditionals Statements/1.if-else statements/2.if-else statements/4.minimumBetween2Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if(num1 < num2) 
+        cout << num1 << " is less than " << num2 << endl;
+    else {
+        if(num1 > num2) 
+            cout << num2 << " is less than " << num1 << endl;
+        else
+            cout << "Both are equal" << endl;
+    }
+
+  return 0;
+}
