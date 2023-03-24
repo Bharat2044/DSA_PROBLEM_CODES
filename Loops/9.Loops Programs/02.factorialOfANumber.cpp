876498b1cd45f4ed12ff39af31581318cb478347
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-24 00:41:16 +0000

    factorial of a number code

---
+++ Loops/9.Loops Programs/02.factorialOfANumber.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/* Approach - 1 : Using Loops  =>  Time Complexity: O(n), Space Complexity: O(1)
+int factorial(int num) {
+    int fact = 1;
+    
+    for(int i = 1; i <= num; ++i) {
+        fact *= i;
+    }
+    
+    return fact;
+}*/
+
+// Approach - 1 : Using Loops  =>  Time Complexity: O(n), Space Complexity: O(n)
+int factorial(int num) {
+    if(num == 0) 
+        return 1;
+    
+    return factorial(num - 1) * num;
+}
+
+int main() {
+    int num;
+    
+    cout << "Enter a positive number: ";
+    cin >> num;
+    
+    if(num < 0) {
+        cout << "Factorial of negative number is not defined 😜";
+    }
+    else {
+        int fact = factorial(num);
+        
+        cout << num << "! = " << fact << endl;
+    }
+    
+    return 0;
+}
