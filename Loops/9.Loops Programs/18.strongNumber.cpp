2a3e632125bdd69e169412bdc5a8d6049fd7ba86
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-28 11:01:02 +0000

    check number is strong number or not code

---
+++ Loops/9.Loops Programs/18.strongNumber.cpp
@@ -0,0 +1,41 @@
+// Strong Numbers: A Strong number is a number, where the sum of the factorial of the digits is equal to the number itself. 1, 2, 145, and 40585 are some examples of Strong numbers.
+// i/p: 145
+// o/p: 145 is a strong number.         => 1! + 4! + 5! = 145
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+
+    return fact;
+}
+
+// Time Complexity: O(n), Space Complexity: O(1)
+bool isStrong(int n) {
+    int sum = 0, temp = n;
+
+    while(n > 0) {
+        int d = n % 10;
+            
+        sum += factorial(d);
+        n /= 10;
+    }
+
+    return sum == temp;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(isStrong(n))
+        cout << n << " is a strong number.";
+    else
+        cout << n << " is not a strong number.";
+
+    return 0;
+}
