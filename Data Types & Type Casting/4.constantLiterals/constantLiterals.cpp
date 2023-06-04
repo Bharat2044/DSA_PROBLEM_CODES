e2a1dafcd38dfe4ccebef9724182ec3ddb9ead55
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-05 01:39:43 +0530

    constant literals code added

---
+++ Data Types & Type Casting/4.constantLiterals/constantLiterals.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // integer literals
+    int a = 4, b = 64, c = -90;
+    
+    // decimal literals
+    float d = 3.14, e = 12.0, f = 0.3333;
+    
+    // character literals
+    char g = 'a', h = '6', i = '@', j = '\n';
+    
+    // string literals
+    string k = "Bharat", l = "abc@gmail.com";
+    
+    
+    cout << a << "  " << b << "  " << c << "\n\n";
+    cout << d << "  " << e << "  " << f << "\n\n";
+    cout << g << "  " << h << "  " << i << "  " << j << "\n\n";
+    cout << k << "  " << l << "\n\n";
+    
+    
+
+    return 0;
+}
