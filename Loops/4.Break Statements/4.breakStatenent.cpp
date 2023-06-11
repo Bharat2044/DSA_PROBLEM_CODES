ae43820b488788be8c96dd2e3d3b558675de2a67
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 09:26:55 +0530

    break statement example code added

---
+++ Loops/4.Break Statements/4.breakStatenent.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    for(int i=1; i<=5; i++) {
+        cout << "For i = " << i << " ===>\n";
+        
+        for(int j=1; j<=5; j++) {
+            if(j%2 == 0)
+                break;
+            cout << j << " ";
+        }
+        cout << endl;
+        
+        for(int k=11; k<=15; k++) 
+            cout << k << " ";
+        
+        
+        cout << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
