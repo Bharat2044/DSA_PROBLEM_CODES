11d464d9a29312d46d81faa34390b25b937a3f7e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 08:26:06 +0530

    print multiple output in single line code added

---
+++ Input-Output and Constant Literals and Fast Input-Output/1.Input-Output/2.printMultipleOutputInSingleLine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    float b = 4.5f;
+    char c = 'd';
+    
+    cout << "a = " << a << "\nb = " << b << "\nc = " << c << endl;
+    
+
+    return 0;
+}
