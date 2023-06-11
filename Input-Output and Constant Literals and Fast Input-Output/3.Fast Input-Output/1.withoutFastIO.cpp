d3cb83542ba4b038cca2a8fcc70f3c33c1989e9d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 08:34:35 +0530

    without fast i/o code added

---
+++ Input-Output and Constant Literals and Fast Input-Output/3.Fast Input-Output/1.withoutFastIO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    cout << "1\n";
+    printf("2\n");
+    cout << "3\n";
+    printf("4\n");
+                          /*
+                          1
+                          2
+                          3
+                          4*/
+
+    return 0;
+}
