21c75e24df4254e81098f6d39111083d4e98cfb8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 08:45:58 +0530

    with fast i/o code added

---
+++ Input-Output and Constant Literals and Fast Input-Output/3.Fast Input-Output/5.withFastIOEx4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);    cin.tie(NULL);
+    
+    int a, b;
+    
+    cout << "Enter the value of a: ";
+    cin >> a;
+    
+    cout << "Enter the value of a: ";
+    cin >> b;
+    
+    cout << "a + b = " << (a + b) << endl;
+    
+/*
+Input:
+    2
+    3
+Output:
+    Enter the value of a: Enter the value of a: a + b = 5
+*/
+
+    return 0;
+}
