1e9b3c031a8f8a8b5a67f1f7c1c4c65db6aaa1e0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 08:20:32 +0530

    code added

---
+++ Input-Output and Constant Literals and Fast Input-Output/1.Input-Output/3.takingInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    
+    cout << "Enter value of a: ";
+    cin >> a;
+    
+    cout << "Enter value of b: ";
+    cin >> b;
+    
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
