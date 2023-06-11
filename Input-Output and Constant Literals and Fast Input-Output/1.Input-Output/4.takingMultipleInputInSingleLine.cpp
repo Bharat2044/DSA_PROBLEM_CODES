9c66ca1af3c968e9edb8b24718ceb1dc1b051175
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 08:23:26 +0530

    taking multiple input in single line code added

---
+++ Input-Output and Constant Literals and Fast Input-Output/1.Input-Output/4.takingMultipleInputInSingleLine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    char c;
+    
+    cout << "Enter value of a, b, c: ";
+    cin >> a >> b >> c;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    return 0;
+}
