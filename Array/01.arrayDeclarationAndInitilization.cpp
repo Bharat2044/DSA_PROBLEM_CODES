0524aae2af4661c0f35da5c8e977804ed675d8d9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-11 07:05:17 +0530

    array declaration and initialization code added

--- Array/01.arrayDeclarationAndInitilization.cpp
+++ Array/01.arrayDeclarationAndInitilization.cpp
@@ -1,5 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++)
+        cout << arr[i] << "  ";
+        
+    cout << endl << endl;
+}
 
 int main() {
     int A[5];                               // Array Declaration
@@ -7,22 +14,53 @@ int main() {
     int C[5] = {2, 4};                      // Array Declaration + Initialization
     int D[5] = {0};                         // Array Declaration + Initialization
     int E[] = {2, 4, 6, 8, 10};             // Array Declaration + Initialization
+        
+    cout << "Array A elements are: ";
+    printArray(A, 5);
+        
+    cout << "Array B elements are: ";
+    printArray(B, 5);
+        
+    cout << "Array C elements are: ";
+    printArray(C, 5);
+        
+    cout << "Array D elements are: ";
+    printArray(D, 5);
+        
+    cout << "Array E elements are: ";
+    printArray(E, sizeof(E)/sizeof(E[0]));
     
-    for(int i=0; i<5; i++)
-        cout << A[i] << "   ";
-    cout<<endl;
-    for(int i=0; i<5; i++)
-        cout << B[i] << "   ";
-    cout<<endl;
-    for(int i=0; i<5; i++)
-        cout << C[i] << "   ";
-    cout<<endl;
-    for(int i=0; i<5; i++)
-        cout << D[i] << "   ";
-    cout<<endl;
-    for(int i=0; i<5; i++)
-        cout << E[i] << "   ";
-    cout<<endl;
     
+    // Taking input using normal for loop
+    int n1;
+    cout << "Enter size of array arr1: ";
+    cin >> n1;
+    
+    int arr1[n1];
+    
+    cout << "Enter array arr1 elements:\n";
+    for(int i=0; i<n1; i++)
+        cin >> arr1[i];
+        
+    cout << "Array arr1 elements are: ";
+    printArray(arr1, n1);
+    
+    
+    // Taking input using for-each loop
+    int n2;
+    cout << "Enter size of array arr2: ";
+    cin >> n2;
+    
+    int arr2[n2];
+    
+    cout << "Enter array arr2 elements:\n";
+    for(auto &ele : arr2)
+        cin >> ele;
+        
+    cout << "Array arr2 elements are: ";
+    for(auto ele : arr2)
+        cout << ele << "  ";
+    cout << endl << endl;
+
     return 0;
 }
